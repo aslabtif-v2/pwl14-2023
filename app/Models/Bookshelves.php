2483f9ae02b71a492e22f4a29521af41066dc4cc
ASLABTIF (UNSUR) <labtif.unsur@gmail.com>
2026-04-27 14:49:28 +0700

    Update Bookshelves.php

--- app/Models/Bookshelves.php
+++ app/Models/Bookshelves.php
@@ -6,6 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Bookshelves extends Model
 {
+       protected $fillable = [
+        'code',
+        'name',
+    ];
+
     public function books()
     {
         return $this->hasMany(Book::class, 'bookshelf_id', 'id');

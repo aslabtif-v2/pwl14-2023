6ab2eae141769d7edb3f8b876bec652da9dfd19a
ASLABTIF (UNSUR) <labtif.unsur@gmail.com>
2026-04-27 14:49:05 +0700

    Add fillable properties to Book

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -6,6 +6,16 @@ use Illuminate\Database\Eloquent\Model;
 
 class Book extends Model
 {
+     protected $fillable =[
+        'title',
+        'author',
+        'year',
+        'publisher',
+        'city',
+        'cover',
+        'bookshelf_id',
+    ];
+
     public function bookshelf()
     {
         return $this->belongsTo(Bookshelves::class, 'bookshelf_id', 'id');
